957fdd865b5a4b365657ca8257d4e10044fd184d
Jonathan McCrary <jmccrary@westmonroe.com>
2022-08-02 17:00:42 -0500

    Initial checkin

---
+++ Built_ExcelAddIn/Built_ExcelAddIn/ExcelRibbonUDFAddin.cs
@@ -0,0 +1,22 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Built_ExcelAddIn
+{
+    public class ExcelRibbonUDFAddin : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            // startup code
+        }
+
+        public void AutoClose()
+        {
+            // clean up
+        }
+    }
+}
---
+++ Built_ExcelAddIn/Built_ExcelAddIn/RibbonController.cs
@@ -0,0 +1,41 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.Integration.CustomUI;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Runtime.InteropServices;
+
+namespace Built_ExcelAddIn
+{
+    [ComVisible(true)]
+    public class RibbonController : ExcelRibbon, IDisposable
+    {
+        private Microsoft.Office.Core.IRibbonUI _ribbonUi;
+
+        private Application App
+        {
+            get => (Application)ExcelDnaUtil.Application;
+        }
+
+        public override string GetCustomUI(string ribbonID) =>
+            @"<customUI xmlns='http://schemas.microsoft.com/office/2009/07/customui'>
+          <ribbon>
+             <tabs>
+              <tab id='sample_tab' label='GoTask'>
+                <group id='sample_group' label='Operations'>
+                  <button id='do_reverse_range' label='Reverse' size='large' getImage='OnDoReverseGetImage' onAction='OnDoReverse'/>
+                </group>
+              </tab>
+            </tabs>
+          </ribbon>
+        </customUI>";
+
+        public void OnLoad(Microsoft.Office.Core.IRibbonUI ribbonUI)
+        {
+            _ribbonUi = ribbonUI;
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
